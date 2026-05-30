705dff0d227948f2206d6ea2a5cce3933052072b
Ivan M <ivan.saxalin@mail.ru>
2026-05-30 16:53:57 +0300

    fix: use manual multipart body in upscale-file.ps1 instead of broken -Form parameter

--- server/upscale-file.ps1
+++ server/upscale-file.ps1
@@ -23,8 +23,28 @@ try {
 }
 
 try {
-    $form = @{ file = Get-Item -LiteralPath $FilePath }
-    $response = Invoke-RestMethod -Uri "$ServerUrl/upscale" -Method Post -Form $form -TimeoutSec 300 -ErrorAction Stop
+    $fileBytes = [System.IO.File]::ReadAllBytes($FilePath)
+    $fileName = [System.IO.Path]::GetFileName($FilePath)
+    $boundary = [System.Guid]::NewGuid().ToString()
+    $LF = "`r`n"
+
+    $bodyLines = @(
+        "--$boundary",
+        "Content-Disposition: form-data; name=`"file`"; filename=`"$fileName`"",
+        "Content-Type: application/octet-stream",
+        "",
+        ""
+    )
+    $headerBytes = [System.Text.Encoding]::UTF8.GetBytes(($bodyLines -join $LF))
+    $footerBytes = [System.Text.Encoding]::UTF8.GetBytes("${LF}--${boundary}--${LF}")
+
+    $body = New-Object byte[] ($headerBytes.Length + $fileBytes.Length + $footerBytes.Length)
+    [Buffer]::BlockCopy($headerBytes, 0, $body, 0, $headerBytes.Length)
+    [Buffer]::BlockCopy($fileBytes, 0, $body, $headerBytes.Length, $fileBytes.Length)
+    [Buffer]::BlockCopy($footerBytes, 0, $body, $headerBytes.Length + $fileBytes.Length, $footerBytes.Length)
+
+    $contentType = "multipart/form-data; boundary=$boundary"
+    $response = Invoke-RestMethod -Uri "$ServerUrl/upscale" -Method Post -Body $body -ContentType $contentType -TimeoutSec 300 -ErrorAction Stop
 } catch {
     Add-Type -AssemblyName System.Windows.Forms
     [System.Windows.Forms.MessageBox]::Show(
